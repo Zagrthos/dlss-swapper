80505c52b200c33dbc5b54c534c5b44c9b7c4fd8
Zagrthos <36556009+Zagrthos@users.noreply.github.com>
2024-08-21 23:39:38 +0200

    Formatting of DLSSPickerControl

--- src/UserControls/DLSSPickerControl.xaml.cs
+++ src/UserControls/DLSSPickerControl.xaml.cs
@@ -1,19 +1,7 @@
-﻿using DLSS_Swapper.Data;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using MvvmHelpers;
-using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using DLSS_Swapper.Data;
+using Microsoft.UI.Xaml.Controls;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -22,8 +10,8 @@ namespace DLSS_Swapper.UserControls
 {
     public sealed partial class DLSSPickerControl : UserControl
     {
-        Game _game;
-        public List<DLSSRecord> DLSSRecords { get; } = new List<DLSSRecord>();
+        private readonly Game _game;
+        public List<DLSSRecord> DLSSRecords { get; } = [];
 
         public DLSSPickerControl(Game game)
         {
